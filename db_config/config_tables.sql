f78a0e9d7579bd3cf4ead6dd3d77431a0e97eaf1
Ashton <90576442+agstatz@users.noreply.github.com>
2021-09-30 15:49:47 -0400

    fixing user and game

--- db_config/config_tables.sql
+++ db_config/config_tables.sql
@@ -17,16 +17,13 @@ DROP TABLE IF EXISTS leaderboard;
 -- Creating Tables
 
 CREATE TABLE user (
-    userID              INT(10) NOT NULL AUTO_INCREMENT,
+    userID              INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username            VARCHAR2(255) NOT NULL,
-    password            VARCHAR2(64) NOT NULL,
-    
+    password            VARCHAR2(64) NOT NULL
 );
 
-ALTER TABLE user ADD CONSTRAINT user_pk PRIMARY KEY ( userID );
-
 CREATE TABLE game (
-    gameID              INT(10) NOT NULL AUTO_INCREMENT,
+    gameID              INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     winnerSore          NUMBER(10, 0),
-    gameDate            DATETIME
+    gameDate            DATE
 );

97eb905e0cbfe4f934add9f532cef160a4171052
nicballesteros <nicballesteros2@gmail.com>
2021-09-30 10:29:19 -0400

    connect node to mysql

--- backend/main.js
+++ backend/main.js
@@ -3,6 +3,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'faceoff-db',
+    user     : 'faceoff',
+    password : 'FaceoffAIRocks',
+    database : 'faceoff'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(`Connected to MySQL as theadID: ${connection.threadId}`);
+});
+
 app.get('/', (req, res) => {
     res.send('hello world');
 });

6b452cec5864aeebcac690d2abb534f98eda16a9
nicballesteros <nicballesteros2@gmail.com>
2021-10-09 20:56:24 -0400

    create gen key script

---
+++ backend/keys/genKeys.js
@@ -0,0 +1,29 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+/**
+ * @description This function creates an RSA256 public private key pair. It then 
+ * puts it into two seperate files. 
+ */
+
+function genKeyPair() {
+    //Generate a key pair.
+    const keyPair = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem',
+        },
+    });
+
+    //Write the keys to files.
+    fs.writeFileSync(`${__dirname}/id_rsa_pub.pem`, keyPair.publicKey);
+
+    fs.writeFileSync(`${__dirname}/id_rsa_priv.pem`, keyPair.privateKey);
+}
+
+genKeyPair();

d2a83162688d9b56824292f576d39aef0b08f8fa
nicballesteros <nicballesteros2@gmail.com>
2021-10-10 21:56:18 -0400

    change username to user

--- backend/api/user/register.js
+++ backend/api/user/register.js
@@ -22,17 +22,17 @@ const validateRegisterBody = (body) => {
     if (payload.email.match(/\S+\@+\S+\.+\S/) == null)
         throw new BadRequestError('Email malformed.', 400);
 
-    if (typeof payload.username !== 'string') 
-        throw new BadRequestError('Username must be of type string.', 400);
+    if (typeof payload.user !== 'string') 
+        throw new BadRequestError('User must be of type string.', 400);
 
-    if (payload.username.length === 0) 
-        throw new BadRequestError('Username must not be an empty string.', 400);
+    if (payload.user.length === 0) 
+        throw new BadRequestError('User must not be an empty string.', 400);
 
-    if (payload.username.match(/^\S*$/) == null) 
-        throw new BadRequestError('Username must not have whitespace.', 400);
+    if (payload.user.match(/^\S*$/) == null) 
+        throw new BadRequestError('User must not have whitespace.', 400);
 
-    if (payload.username.match(/@/) != null) 
-        throw new BadRequestError('Username must not have an \'@\' symbol.', 400)
+    if (payload.user.match(/@/) != null) 
+        throw new BadRequestError('User must not have an \'@\' symbol.', 400)
 
     if (typeof payload.password !== 'string')
         throw new BadRequestError('Password must be of type string.', 400);

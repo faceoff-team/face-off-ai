e399beb42bed0f6934a40522c197a7445d8e6976
Ashton <90576442+agstatz@users.noreply.github.com>
2021-10-15 01:01:15 -0400

    Changed Profile UI and Navbar UI

--- frontend/face-off-ai/src/components/NavigationBar.jsx
+++ frontend/face-off-ai/src/components/NavigationBar.jsx
@@ -10,55 +10,53 @@ function NavigationBar(props) {
     return (
         <div className="navBar">
             <nav class="navbar navbar-expand navbar-dark">
-                <div class="container">
-                    <Link class="navbar-brand" to="/home">
-                        <img class="small-logo" alt="FACEOFF AI" src={logo}></img>
-                    </Link>
-                    <div>
-                        <ul class="navbar-nav ml-auto">
-                            <li
-                                class={`nav-item ${
-                                    props.location.pathname === "/" ? "active" : ""
+                <Link class="navbar-brand" to="/home">
+                    <img class="small-logo" alt="FACEOFF AI" src={logo}></img>
+                </Link>
+                <div class="navbar-align-right">
+                    <ul class="navbar-nav ml-auto">
+                        <li
+                            class={`nav-item ${
+                                props.location.pathname === "/" ? "active" : ""
+                            }`}
+                        >
+                            <Link class="nav-link" to="/home">
+                                <HomeIcon>
+                                    <span class="sr-only">(current)</span>
+                                </HomeIcon>
+                            </Link>
+                        </li>
+                        <li
+                            class={`nav-item ${props.location.pathname === "/leaderboards" ? "active" : ""
                                 }`}
-                            >
-                                <Link class="nav-link" to="/home">
-                                    <HomeIcon>
-                                        <span class="sr-only">(current)</span>
-                                    </HomeIcon>
-                                </Link>
-                            </li>
-                            <li
-                                class={`nav-item ${props.location.pathname === "/leaderboards" ? "active" : ""
-                                    }`}
-                            >
-                                <Link class="nav-link" to="/leaderboards">
-                                    <LeaderboardIcon>
-                                        <span class="sr-only">(current)</span>
-                                    </LeaderboardIcon>
-                                </Link>
-                            </li>
-                            <li
-                                class={`nav-item ${props.location.pathname === "/profile" ? "active" : ""
-                                    }`}
-                            >
-                                <Link class="nav-link" to="/profile">
-                                    <AccountCircleIcon>
-                                        <span class="sr-only">(current)</span>
-                                    </AccountCircleIcon>
-                                </Link>
-                            </li>
-                            <li
-                                class={`nav-item ${props.location.pathname === "/settings" ? "active" : ""
-                                    }`}
-                            >
-                                <Link class="nav-link" to="/settings">
-                                    <SettingsIcon>
-                                        <span class="sr-only">(current)</span>
-                                    </SettingsIcon>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                        >
+                            <Link class="nav-link" to="/leaderboards">
+                                <LeaderboardIcon>
+                                    <span class="sr-only">(current)</span>
+                                </LeaderboardIcon>
+                            </Link>
+                        </li>
+                        <li
+                            class={`nav-item ${props.location.pathname === "/profile" ? "active" : ""
+                                }`}
+                        >
+                            <Link class="nav-link" to="/profile">
+                                <AccountCircleIcon>
+                                    <span class="sr-only">(current)</span>
+                                </AccountCircleIcon>
+                            </Link>
+                        </li>
+                        <li
+                            class={`nav-item ${props.location.pathname === "/settings" ? "active" : ""
+                                }`}
+                        >
+                            <Link class="nav-link" to="/settings">
+                                <SettingsIcon>
+                                    <span class="sr-only">(current)</span>
+                                </SettingsIcon>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>

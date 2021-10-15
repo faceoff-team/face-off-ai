f859127bc4152970c74a595891f21b48aeb0cf35
Ashton <90576442+agstatz@users.noreply.github.com>
2021-10-14 21:31:42 -0400

    Adding resizable profile page

--- frontend/face-off-ai/src/components/SmallProfile.jsx
+++ frontend/face-off-ai/src/components/SmallProfile.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Avatar from "@mui/material/Avatar";
+import Grid from '@mui/material/Grid';
 
 function SmallProfile({ username, picture }) {
     return (
         <div class="small-profile">
-            <div class="column">
-                <div class="row">
-                    <Avatar
-                        src={picture}
-                        sx={{ bgcolor: "#23430" }}>
-                    </Avatar>
-                    <div class="small-profile-text">{username}</div>
-                </div>
+            <div class="row">
+                <Grid container>
+                    <Grid>
+                        <Avatar
+                            src={picture}
+                            sx={{ bgcolor: "#23430" }}>
+                        </Avatar>
+                    </Grid>
+                    <Grid>
+                        <div class="small-profile-text">{username}</div>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
--- frontend/face-off-ai/src/screens/Profile.jsx
+++ frontend/face-off-ai/src/screens/Profile.jsx
@@ -5,6 +5,7 @@ import ProfileBody from "../components/ProfileBody.jsx"
 import HorizontalLine from "../components/HorizontalLine.jsx";
 import AccountList from "../components/AccountList.jsx";
 import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box";
 
 /* This picture is a placeholder for our presentation */
 import Picture3 from "../assets/profile-picture-2.jpg";
@@ -12,7 +13,7 @@ import Picture3 from "../assets/profile-picture-2.jpg";
 function Profile() {
     return (
         <div className="Profile" class="container">
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} align-items="center">
                 <Grid item xs={4} sm={6} md={8}>
                     <div class="basic-container col">
                         <ProfileHeader username="MustardMan900" picture={Picture3} bio="Howdy yall I'm mr. musterd man"/>
@@ -20,13 +21,20 @@ function Profile() {
                         <ProfileBody />
                     </div>
                 </Grid>
-                <Grid item xs={2} sm={4} md={4}>
+                <Box
+                    component={Grid}
+                    item
+                    xs={2}
+                    sm={3}
+                    md={4}
+                    display={{ xs: "none", sm: "block", md: "block" }}
+                >
                     <div>
                         <div class="basic-container">
                             <AccountList title="You might know" />
                         </div>
                     </div>
-                </Grid>
+                </Box>
             </Grid>
         </div>
     );

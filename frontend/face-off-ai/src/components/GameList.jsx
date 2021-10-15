81083a63acc41fe1311eae1466cc9ba21984b2ac
Ashton <90576442+agstatz@users.noreply.github.com>
2021-10-15 02:30:59 -0400

    Added additional functionality to SmallGame components

--- frontend/face-off-ai/src/components/GameList.jsx
+++ frontend/face-off-ai/src/components/GameList.jsx
@@ -7,27 +7,27 @@ import Grid from '@mui/material/Grid';
 function GameList({ title, username }) {
     return (
         <div>
-            
+
             <h1 class="font-weight-heavy-small">{username}'s {title}</h1>
             <HorizontalLine color="#f7f7f7" width="100%" />
             <div class="column">
                 <div class="row">
-                    <SmallGame date="09/14/2021" gamehighscore="23000" />
+                    <SmallGame date="09/14/2021" gamehighscore="23000" result="true" />
                 </div>
                 <div class="row">
-                    <SmallGame date="09/12/2021" gamehighscore="19400" />
+                    <SmallGame date="09/12/2021" gamehighscore="19400" result="false" />
                 </div>
                 <div class="row">
-                    <SmallGame date="08/10/2021" gamehighscore="19493" />
+                    <SmallGame date="08/10/2021" gamehighscore="19493" result="false" />
                 </div>
                 <div class="row">
-                    <SmallGame date="02/23/2021" gamehighscore="12000" />
+                    <SmallGame date="02/23/2021" gamehighscore="12000" result="true"/>
                 </div>
                 <div class="row">
-                    <SmallGame date="10/23/2020" gamehighscore="14200" />
+                    <SmallGame date="10/23/2020" gamehighscore="14200" result="false"/>
                 </div>
                 <div class="row">
-                    <SmallGame date="10/19/2020" gamehighscore="12300" />
+                    <SmallGame date="10/19/2020" gamehighscore="12300" result="true"/>
                 </div>
             </div>
         </div>
--- frontend/face-off-ai/src/components/SmallGame.jsx
+++ frontend/face-off-ai/src/components/SmallGame.jsx
@@ -2,13 +2,38 @@ import React from "react";
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
 import Box from "@mui/material/Box";
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
 
 
-function SmallGame({ date, gamehighscore }) {
+function ResultIcon(props) {
+    
+    const won = props.win;
+
+    if (won === "true") {
+        const winner = "Won"
+        return (    
+                <Grid>
+                    <span> Result: {winner}</span>
+                    <ControlPointIcon style={{ color: "#358679", marginLeft: "5px" }} />
+                </Grid>
+        );
+    } else {
+        const loser = "Lost"
+        return (
+            <Grid>
+                <span> Result: {loser}</span>
+                <IndeterminateCheckBoxIcon style={{ color: "#a32643", marginLeft: "5px" }} />
+            </Grid>
+        );
+        
+    }
+}
+
+function SmallGame({ date, gamehighscore, result }) {
     return (
         <div class="small-game">
             <div class="row">
-                
                     <Grid container>
                         <Grid>
                             {date}
@@ -26,6 +51,17 @@ function SmallGame({ date, gamehighscore }) {
                         <Grid>
                             <div> High Score: {gamehighscore}</div>
                         </Grid>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                borderRight: '0.1em solid white',
+                                marginRight: '10px',
+                                marginLeft: '10px'
+                            }}
+                        >
+                            <Divider orientation="vertical" flexItem />
+                        </Box>
+                        <ResultIcon win={result} />
                     </Grid>
             </div>
         </div>

be8624ecf724f91726e010546c133f86154d72a4
dommiller88 <dommiller88@gmail.com>
2021-10-15 11:54:10 -0400

    added manual transform test and added arg parsing to transform script

--- model/scripts/transform.py
+++ model/scripts/transform.py
@@ -4,6 +4,7 @@ from os import path, walk
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
+import argparse
 
 face_detect = dlib.get_frontal_face_detector()
 landmark_predict = dlib.shape_predictor('/Users/dommiller88/Documents/GitHub/face-off-ai/model/scripts/shape_predictor_68_face_landmarks.dat')
@@ -36,28 +37,37 @@ def find_nth(haystack, needle, n):
 
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src', required=True)
+    parser.add_argument('--dst', required=True)
+    args = parser.parse_args()
+    print(args.src)
+    for subdir, dirs, files in walk(args.src):
+        for file in files:
+            print(file)
+            if (file.endswith('.jpg')):
+                prefix = subdir[:(find_nth(subdir, '/', 7))]
+                suffix = subdir[find_nth(subdir, '/', 8) + 1:]
+                full_path = path.join(prefix, 'modified_data', suffix, file)
+                image = cv2.imread(path.join(subdir, file))
+                face = face_detect(image, 1)
+                crop_image = Image.open(path.join(subdir, file))
+                for i, face_rect in enumerate(face):
+                    crop_area = (face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom())
+                    cropped = crop_image.crop(crop_area)
+                    cropped = cropped.resize((128,128))
+                    dest_true = path.join(args.dst, file)
+                    cropped.save(dest_true)
+                    image = cv2.imread(dest_true)
+                    image, landmarks = get_landmarks(image)
+                    if image is not None:
+                        image_copy = image_landmarks(image, landmarks)
+                        cv2.imwrite(dest_true, image_copy)
+                    else:
+                        pass
+    
 
-for subdir, dirs, files in walk('/Users/dommiller88/Documents/GitHub/face-off-ai/model/data'):
-    for file in files:
-        if (file.endswith('.jpg')):
-            prefix = subdir[:(find_nth(subdir, '/', 7))]
-            suffix = subdir[find_nth(subdir, '/', 8) + 1:]
-            full_path = path.join(prefix, 'modified_data', suffix, file)
-            image = cv2.imread(path.join(subdir, file))
-            face = face_detect(image, 1)
-            crop_image = Image.open(path.join(subdir, file))
-            for i, face_rect in enumerate(face):
-                crop_area = (face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom())
-                cropped = crop_image.crop(crop_area)
-                cropped = cropped.resize((128,128))
-                cropped.save(full_path)
-                image = cv2.imread(full_path)
-                image, landmarks = get_landmarks(image)
-                if image is not None:
-                    image_copy = image_landmarks(image, landmarks)
-                    cv2.imwrite(full_path, image_copy)
-                else:
-                    pass
             
     
         

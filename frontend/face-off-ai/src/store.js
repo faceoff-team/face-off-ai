f26d9ada5e611cd02500eda97d645f0fdcb02d3d
nicballesteros <nicballesteros2@gmail.com>
2021-10-16 13:25:31 -0400

    fix store export statements

--- frontend/face-off-ai/src/store.js
+++ frontend/face-off-ai/src/store.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 //Make http accessable throughout app.
-const http = require('axios');
+export const http = require('axios');
 
 http.defaults.headers.post['Content-Type'] = 'application/json';
  
@@ -13,8 +13,7 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENTION__ && window.__REDUX_DEVTOOLS_EXTENTION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ));
 
 export default store;
-export default http;

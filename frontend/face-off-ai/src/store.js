c65af1be414a8210961d200d7957cf9506aa807a
nicballesteros <nicballesteros2@gmail.com>
2021-10-21 19:36:04 -0400

    add devtools extension

--- frontend/face-off-ai/src/store.js
+++ frontend/face-off-ai/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 //Make http accessable throughout app.
 export const http = require('axios');
@@ -13,9 +14,8 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
+const store = createStore(rootReducer, initialState, composeWithDevTools(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ));
 
 export default store;

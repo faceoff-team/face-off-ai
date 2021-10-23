976b57b86806181bd07d849dd9a1b6003d5f02f3
sripotturu <54682689+sripotturu@users.noreply.github.com>
2021-10-23 11:05:36 -0400

    created new theme folder/file

---
+++ frontend/face-off-ai/src/theme/theme-context.js
@@ -0,0 +1,12 @@
+const themes = {
+    dark: {
+        backgroundColor: 'black',
+        backgroundCard: '#25282c',
+        color: 'white'
+      },
+     light: {
+       backgroundColor: 'white',
+       backgroundCard: '#fff',
+       color: 'black'
+     }
+}

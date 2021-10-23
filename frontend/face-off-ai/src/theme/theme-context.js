a1321b0a7d63ebae8e59ee4e2318a766c3d5ca2f
sripotturu <54682689+sripotturu@users.noreply.github.com>
2021-10-23 11:58:20 -0400

    created ThemeProvider functions and exports

--- frontend/face-off-ai/src/theme/theme-context.js
+++ frontend/face-off-ai/src/theme/theme-context.js
@@ -20,3 +20,23 @@ const themes = {
        color: 'black'
      }
 }
+
+function ThemeProvider({ children }) {
+    const [dark, setDark] = React.useState(false) // Default theme is light
+    
+    // To toggle between dark and light modes
+    const toggle = () => {
+       setDark(!dark)
+    }
+    
+    // Filter the styles based on the theme selected
+    const theme = dark ? themes.dark : themes.light
+    
+    return (
+        <ThemeContext.Provider value={{ theme, dark, toggle }}>
+           {children}
+        </ThemeContext.Provider>
+        )
+}
+
+export { ThemeProvider, ThemeContext }

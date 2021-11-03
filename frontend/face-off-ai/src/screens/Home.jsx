1833c1884ce4f1f6fce10bd2492cf4676f30c093
Ashton <90576442+agstatz@users.noreply.github.com>
2021-11-02 21:24:45 -0400

    Removed Search Bar from Home

--- frontend/face-off-ai/src/screens/Home.jsx
+++ frontend/face-off-ai/src/screens/Home.jsx
@@ -9,13 +9,7 @@ import { Link, withRouter } from 'react-router-dom';
 function Home() {
     return (
         <div className="home" class="container">
-            <TextField
-                label={'Looking for a video?'}
-                placeholder={'Search'}
-                margin={'normal'}
-                color={"primary"}
-                
-            />
+            {/*Add a search bar here */}
             <h1 class="font-weight-heavy" style={{ marginTop: "20px" }}>Welcome! Want to jump right in? Click Below:</h1>
             <HorizontalLine color="#f7f7f7" width="100%"/>
             <div style={{textAlign: 'center'}}>

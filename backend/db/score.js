eb690194f4c663bbc1a35071fa78d2da4c32e5da
nicballesteros <nicballesteros2@gmail.com>
2021-11-03 20:52:50 -0400

    add update function

--- backend/db/score.js
+++ backend/db/score.js
@@ -5,33 +5,31 @@
  * 10/22/21
  */
 
+const { queryPromise } = require(".");
+
 const getUserScores = async (user) => {
-  console.log(`
-    SELECT * FROM user_game
-    WHERE user = ${user.userID};
-  `);
+  let query = `SELECT * FROM user_game WHERE user = ${user.userID}`;
 
-  try {
-    let user = await new Promise((resolve, reject) => {
-      global.connection.query(`
-        SELECT * FROM user_game
-        WHERE user = ${user.userID};
-      `);
-
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve({
-        results,
-        fields,
-      });
-    });
-  } catch (err) {
-    console.error(err);
-  }
+  let userScores = await queryPromise(query);
+  
+  return userScores.results;
 }
+
+/**
+ * This function updates a user_game with the score passed.
+ *  
+ * @param {Number} id The id of the user_game object. 
+ * @param {Number} score The score to update the user_game with. 
+ */
+
+const addScoreToGame = async (id, score) => {
+  let query = 
+    `UPDATE user_game 
+     SET score = ${score}
+     WHERE gameID = ${id}`;
+
+  let game = await queryPromise(query);
+};
 
 module.exports = {
   getUserScores,

505f0c74063c700d04e78be5e97f6807454a3361
nicballesteros <nicballesteros2@gmail.com>
2021-11-03 20:08:58 -0400

    finish out the api endpoints.

--- backend/api/challenge/get.js
+++ backend/api/challenge/get.js
@@ -3,19 +3,34 @@
  * @description This file handles all get requests for challenges
  */
 
+const { getChallenge, getAllChallenges } = require("../../db/challenge");
+
 const handleGetAllChallengesRequest = async (req, res) => {
+  let challenges = await getAllChallenges();
+
   res.status(200).json({
     success: true,
-    msg: `Challenges retreived successfully.`
+    msg: `Challenges retreived successfully.`,
+    challenges,
   });
 };
+
+/**
+ * This function handles all get challenge requests for a specific challenge.
+ *  
+ * @param {Object} req 
+ * @param {Object} res 
+ */
 
 const handleGetChallengeRequest = async (req, res) => {
   let challenge = req.params.challenge;
 
+  challenge = getChallenge(challenge);
+
   res.status(200).json({
     success: true,
     msg: `Challenge retrieved successfully.`,
+    challenge,
   });
 };
 

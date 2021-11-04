b8251a928d01ea9f9170ef48bed88c916af78c85
Ashton <90576442+agstatz@users.noreply.github.com>
2021-11-04 19:28:46 -0400

    Fixed compile error

--- frontend/face-off-ai/src/screens/Leaderboard.jsx
+++ frontend/face-off-ai/src/screens/Leaderboard.jsx
@@ -2,25 +2,16 @@ import React, { useEffect } from "react";
 import LeaderboardList from "../components/LeaderboardList.jsx";
 import { http } from "../store.js";
 
-var userNum;
+
 
 const leaderboard = async() => {
     try {
         let res = await http.get("https://ai.faceoff.cf/api/user/leaderboard");
-
-        console.log("res.data: " + res.data.leaderboard);
         console.log("res json: " + JSON.stringify(res.data.leaderboard));
-        userNum = Object.keys(res.data.leaderboard).length;
-        console.log("size: " + size);
+        var userNum = (res.data.leaderboard).length;
+        console.log("size: " + userNum);
 
-        {/*this.state = {
-            //save individual components here
-            id: res.userid,
-            username: res.username,
-            rank: res.worldRank
-        }*/}
-
-        return res;
+        return userNum;
     } catch (err) {
         console.error(err);
     }
@@ -29,20 +20,12 @@ const leaderboard = async() => {
 
 function Leaderboard(props) {
     leaderboard();
-    
-    // TODO: fix the set state issue
-    useEffect(() => {
-        function handleStatusChange(status) {
-
-        }
-        //this.setState(leaderboard());
-    });
 
     return (
         <div className="leaderboard" class="container">
             <div class="basic-container">
                 <h1 class="font-weight-heavy" style={{ marginTop: "10px" }}>Leaderboard</h1>
-                <LeaderboardList props={props} numUsers={userNum}/>
+                <LeaderboardList props={props} numUsers={3}/>
             </div>
         </div>
     );

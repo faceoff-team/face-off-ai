e7b9cf2f48aa1930129fa2b3bee4512eb6b4319a
Ashton <90576442+agstatz@users.noreply.github.com>
2021-11-03 23:45:42 -0400

    Updated console log statement

--- frontend/face-off-ai/src/screens/Leaderboard.jsx
+++ frontend/face-off-ai/src/screens/Leaderboard.jsx
@@ -8,8 +8,9 @@ const leaderboard = async() => {
     try {
         let res = await http.get("/api/user/leaderboard");
 
-        console.log("Hello, res: " + res.data);
-        console.log("res json: " + JSON.stringify(res.data));
+        console.log("res: " + res);
+        console.log("res.body: " + res.body);
+        console.log("res json: " + JSON.stringify(res.body));
 
         {/*this.state = {
             //save individual components here

f0d6b92c0ae1852fe3b9baf56a80eebeded0f282
nicballesteros <nicballesteros2@gmail.com>
2021-11-12 20:19:19 -0500

    add new oauth code

--- backend/email.js
+++ backend/email.js
@@ -5,16 +5,66 @@
  */
 
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
-let transport = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    XOAuth2: {
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.OAUTHCLIENTID,
+    process.env.OAUTHCLIENTSECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
       user: "faceoffbot81@gmail.com",
+      accessToken,
       clientId: process.env.OAUTHCLIENTID,
       clientSecret: process.env.OAUTHCLIENTSECRET,
+      refreshToken: process.env.REFRESH
     }
-  }
-});
+  });
 
-module.exports = transport;
+  return transporter;
+};
+
+// const oauth2Client = new OAuth2(
+//     process.env.OAUTHCLIENTID,
+//     process.env.OAUTHCLIENTSECRET,
+//     "https://developers.google.com/oauthplayground"
+// );
+
+// oauth2Client.setCredentials({
+//     refresh_token: process.env.REFRESH
+// });
+
+
+// let transport = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     XOAuth2: {
+//       user: "faceoffbot81@gmail.com",
+//       clientId: process.env.OAUTHCLIENTID,
+//       clientSecret: process.env.OAUTHCLIENTSECRET,
+//     }
+//   }
+// });
+
+(async () => {
+  module.exports = await createTransporter();
+})

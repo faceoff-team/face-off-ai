f4dd977e18f947ae0e2ea0cbe62ac860d7b8bb7e
nicballesteros <nicballesteros2@gmail.com>
2021-11-15 19:53:25 -0500

    fix forgot password

--- backend/api/user/forgotPassword.js
+++ backend/api/user/forgotPassword.js
@@ -4,15 +4,32 @@
  * 11/15/21
  */
 
+const { getUserByEmail, getUserbyUsername } = require('../../db/user');
 const { sendMail } = require('../../email');
 const BadRequestError = require('../../error/BadRequestError');
- 
+
 const handleChangePasswordRequest = async (req, res) => {
   if (!req.body.user)
     throw BadRequestError(`No user name given.`, 404);
  
+  let user;
+
+  if (req.body.user.includes('@')) {
+    user = getUserbyEmail(req.body.user);
+  } else {
+    user = getUserbyUsername(req.body.user);
+  }
+
+  if (!user.email) {
+    res.status(200).json({
+      success: true,
+      msg: `Change password request proccessed.`,
+    });
+    return;
+  }
+    
   await sendMail({
-    to: `${req.user.email}`,
+    to: `${user.email}`,
     subject: `Reset Password Request`,
     html: `<p>Go to this link to reset your password. ${'this is a link'}</p>`,
   }); 

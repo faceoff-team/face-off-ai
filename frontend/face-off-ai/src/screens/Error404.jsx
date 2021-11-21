56c6297757ba2a4ad07084b0ba174726fbe2a9b1
Ashton <90576442+agstatz@users.noreply.github.com>
2021-11-20 20:19:17 -0500

    Added icon to 404 page

--- frontend/face-off-ai/src/screens/Error404.jsx
+++ frontend/face-off-ai/src/screens/Error404.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 function Error404() {
     return (
         <div class="container">
             <div class="basic-container">
-                <h1 class="font-weight-heavy" style={{ marginTop: "10px" }}>Oops... 404 Error</h1>
+                <h1 class="font-weight-heavy" style={{ marginTop: "10px" }}>
+                    <ErrorOutlineIcon fontSize="large" /> Oops... 404 Error</h1>
             </div>
         </div>
     );

158b63309599e53dc0724bbbad8327bb82205bfe
nicballesteros <nicballesteros2@gmail.com>
2021-11-21 14:28:07 -0500

    change class to className

--- frontend/face-off-ai/src/components/GameList.jsx
+++ frontend/face-off-ai/src/components/GameList.jsx
@@ -6,25 +6,25 @@ function GameList({ title, username }) {
     return (
         <div>
 
-            <h1 class="font-weight-heavy-small">{username}'s {title}</h1>
+            <h1 className="font-weight-heavy-small">{username}'s {title}</h1>
             <HorizontalLine color="#f7f7f7" width="100%" />
-            <div class="column">
-                <div class="row">
+            <div className="column">
+                <div className="row">
                     <SmallGame date="09/14/2021" gamehighscore="23000" result="true" />
                 </div>
-                <div class="row">
+                <div className="row">
                     <SmallGame date="09/12/2021" gamehighscore="19400" result="false" />
                 </div>
-                <div class="row">
+                <div className="row">
                     <SmallGame date="08/10/2021" gamehighscore="19493" result="false" />
                 </div>
-                <div class="row">
+                <div className="row">
                     <SmallGame date="02/23/2021" gamehighscore="12000" result="true"/>
                 </div>
                 <div class="row">
                     <SmallGame date="10/23/2020" gamehighscore="14200" result="false"/>
                 </div>
-                <div class="row">
+                <div className="row">
                     <SmallGame date="10/19/2020" gamehighscore="12300" result="true"/>
                 </div>
             </div>

aaf17d11694fd753ed978fe942138174d54ef53f
sripotturu <54682689+sripotturu@users.noreply.github.com>
2021-11-21 23:17:00 -0500

    Redid some socket logic

--- frontend/face-off-ai/src/components/socket.js
+++ frontend/face-off-ai/src/components/socket.js
@@ -1,10 +1,13 @@
-/* const express = require(‘express’) // Importing Express
+/* 
+
+const express = require(‘express’) // Importing Express
 const app = express() // Creating Express Server
 const host = ‘localhost’ // Specifying Host
 const port = 8000 // Specifying Port number// Creating Http Server from Express App to work with socket.io
+
 const http = require(‘http’).Server(app);// Initializing socket.io object
 const io = require(‘socket.io’)(http,{
- // Specifying CORS 
+
  cors: {
  origin: “*”,
  }
@@ -28,6 +31,13 @@ function mapDispatchToProps(dispatch) {
   return { 
     getSocket: () => dispatch(getSocket()), 
     getSocketio: () => dispatch(getSocketIO()), 
+    testing: data => dispatch(testFunc(data)) };
+}
+
+function mapDispatchToPropsNew(dispatch) {
+  return { 
+    getSocket: () => dispatch(getSocketRaw()), 
+    getSocketio: () => dispatch(getSocketIORaw()), 
     testing: data => dispatch(testFunc(data)) };
 }
 

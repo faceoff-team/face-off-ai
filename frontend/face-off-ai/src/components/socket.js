17e93f1313f3d1af9078cc58d5fa454bb906f88f
sripotturu <54682689+sripotturu@users.noreply.github.com>
2021-11-21 21:24:32 -0500

    Rough implementation of socket foundation

---
+++ frontend/face-off-ai/src/components/socket.js
@@ -0,0 +1,11 @@
+const express = require(‘express’) // Importing Express
+const app = express() // Creating Express Server
+const host = ‘localhost’ // Specifying Host
+const port = 8000 // Specifying Port number// Creating Http Server from Express App to work with socket.io
+const http = require(‘http’).Server(app);// Initializing socket.io object
+const io = require(‘socket.io’)(http,{
+ // Specifying CORS 
+ cors: {
+ origin: “*”,
+ }
+})

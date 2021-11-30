9e1ec9661a7552ced4bf21178da17e0075d8bca9
nicballesteros <nicballesteros2@gmail.com>
2021-11-30 00:23:09 -0500

    Once again reading exit from echo

--- backend/test/wait-for-db.sh
+++ backend/test/wait-for-db.sh
@@ -4,7 +4,6 @@ docker cp ../../db_config/config_tables.sql test-faceoff-db:/config.sql
 
 while true; do
   docker exec -i test-faceoff-db /bin/bash -c 'mysql -u root -ppassword -h test-faceoff-db < /config.sql; exit $?'
-  echo $?
   if [ $? -eq 0 ]; then
     break
   fi

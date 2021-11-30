ca9a7c08fc9b63d40c74c5da3e42ea4b80cc1920
nicballesteros <nicballesteros2@gmail.com>
2021-11-29 22:57:56 -0500

    debug error code

--- backend/test/wait-for-db.sh
+++ backend/test/wait-for-db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 while true; do
-  docker exec -i test-faceoff-db /bin/sh -c 'mysqladmin ping &> /dev/null; exit $?'
+  docker exec -i test-faceoff-db /bin/sh -c 'mysqladmin ping; exit $?'
+  echo $?
   if [ $? -eq 0 ]; then
     break
   fi

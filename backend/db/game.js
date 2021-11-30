e350718c96dc4bf6943ef75d5e6fa2a44c66b5f9
dommiller88 <dommiller88@gmail.com>
2021-11-30 17:05:22 -0500

    hmm

--- backend/db/game.js
+++ backend/db/game.js
@@ -62,7 +62,7 @@ const getAllUserGames = async (userID) => {
 };
 
 const getAllGames = async () => {
-  let query = `SELECT * FROM game;`;
+  let query = `SELECT * FROM game`;
 
   let games = await queryPromise(query);
 

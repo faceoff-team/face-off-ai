2d6b88905676a0589e9a7e028fa51421b2555530
sripotturu <54682689+sripotturu@users.noreply.github.com>
2021-12-01 14:17:56 -0500

    Cleaned up Game page logic

--- frontend/face-off-ai/src/screens/Game.jsx
+++ frontend/face-off-ai/src/screens/Game.jsx
@@ -37,6 +37,7 @@ function Game(gameID) {
         .then( response => {
             // Do something with response.
             console.log(response);
+
     });
 
     const avgTimeDict = {"r9SsqcT6heE" : "50 seconds", "YqaacQc6sho" : "1 minute 3 seconds"};
@@ -128,32 +129,6 @@ function Game(gameID) {
         }
         return message;
     }
-
-    /*
-    <!-- Trigger the modal with a button -->
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
-    <!-- Modal -->
-    <div id="myModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-
-        <!-- Modal content-->
-        <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-            <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-        </div>
-
-    </div>
-    </div>
-    */
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);

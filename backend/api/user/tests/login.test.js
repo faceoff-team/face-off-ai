12b3d5c47f8066073c095c7ad18cc252038b5bf9
nicballesteros <nicballesteros2@gmail.com>
2021-12-01 17:19:36 -0500

    make testing modular

---
+++ backend/api/user/tests/login.test.js
@@ -0,0 +1,13 @@
+/**
+ * @author Nic Ballesteros
+ * @description This file tests the login function.
+ * 12/1/21
+ */
+
+const { post } = require('../../test/utils.js');
+
+function suite(app) {
+
+}
+
+module.exports = suite;

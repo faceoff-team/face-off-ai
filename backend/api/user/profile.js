b013c1fc5255d2f736f64da396d4cf909c3719e5
Ashton <90576442+agstatz@users.noreply.github.com>
2021-12-01 22:42:35 -0500

    Changed to getUserByUsername

--- backend/api/user/profile.js
+++ backend/api/user/profile.js
@@ -7,7 +7,7 @@
 
 const BadRequestError = require("../../error/BadRequestError");
 
-const { updateProfile, getUserByKey } = require("../../db/user");
+const { updateProfile, getUserByUsername } = require("../../db/user");
 
 const handleGetProfile = async (req, res) => {
     let user = {};

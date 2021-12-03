3e2fbe19d7e6b1b4c34ad0e53eff6be3cb62d6e8
dommiller88 <dommiller88@gmail.com>
2021-12-03 00:00:19 -0500

    fixed axios call

--- frontend/face-off-ai/src/components/WebcamCapture.jsx
+++ frontend/face-off-ai/src/components/WebcamCapture.jsx
@@ -40,11 +40,11 @@ const WebcamCapture = ({running, setRunning, setLossTime, emotion}) => {
                         image: imageSrc
                     })
                     .then((res) => {
-                        console.log("reached then clause")
-                        if (res.success == 'true') {
-                            console.log(res.prediction);
-                            console.log(res.confidence);
-                            if (emotions[res.prediction]== emotion && parseFloat(res.confidence) > 0.7) {
+                        console.log(res)
+                        if (res.data.success == 'true') {
+                            console.log(res.data.prediction);
+                            console.log(res.data.confidence);
+                            if (emotions[res.data.prediction]== emotion && parseFloat(res.data.confidence) > 0.7) {
                                 setLossTime(currentElapsed);
                                 console.log("loss detected")
                                 setRunning(false);

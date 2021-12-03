a5aa71018a28a3ac9d910c832296bcc5ba68d6a3
sripotturu <54682689+sripotturu@users.noreply.github.com>
2021-12-03 12:00:23 -0500

    Added new user score API

--- backend/api/user/scores.js
+++ backend/api/user/scores.js
@@ -5,7 +5,17 @@
  * 10/22/21
  */
 
-const { getUserScores } = require(`../../db/score`);
+const { getUserScores, getScoresForGame } = require(`../../db/score`);
+
+const handleGetGameScores = async (req, res) => {
+  let scores = await getScoresForGame()
+  
+  res.status(200).json({
+    success: true,
+    msg: "User scores retrieved.",
+    scores,
+  });
+};
 
 const handleGetUserScores = async (req, res) => {
   let scores = await getUserScores()
@@ -17,10 +27,19 @@ const handleGetUserScores = async (req, res) => {
   });
 };
 
-module.exports = async (req, res, next) => {
-  try {
-    await handleGetUserScores(req, res);
-  } catch (err) {
-    next(err);
-  }
+module.exports = {
+  userScoreGet:  async (req, res, next) => {
+    try {
+      await handleGetUserScores(req, res);
+    } catch (err) {
+      next(err);
+    }
+  },
+  scoresForGameGet:  async (req, res, next) => {
+    try {
+      await handleGetGameScores(req, res);
+    } catch (err) {
+      next(err);
+    }
+  },
 }
